295c59d64800a223f8b9473e5defae521a666784
ZekiBehailu <zeking02@gmail.com>
2024-12-10 22:19:58 +0300

    Changed site title to Git Master

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
         <div className="flex justify-between h-16">
           <div className="flex items-center">
             <Link href="/" className="text-2xl font-bold text-indigo-600">
-              GitLearn
+              Git Master
             </Link>
           </div>
 
