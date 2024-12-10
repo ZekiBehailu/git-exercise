8b55800d48ee08a7fd4154c1c8fd25b4232fffe6
ZekiBehailu <zeking02@gmail.com>
2024-12-10 22:14:09 +0300

    Enhance documentation site with improved navigation and UI

--- src/components/docs/DocLayout.tsx
+++ src/components/docs/DocLayout.tsx
@@ -1,12 +1,14 @@
 "use client";
 
+import Navbar from "../Navbar";
 import DocSidebar from "./DocSidebar";
 
 export default function DocLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen bg-white">
+      <Navbar />
       <DocSidebar />
-      <div className="pl-64">
+      <div className="pl-64 pt-16">
         <div className="max-w-4xl mx-auto px-6 py-8">{children}</div>
       </div>
     </div>
